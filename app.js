280fe4869c149b08f696d26ef96e9735ffcb0a4c
akerke150892 <aks150892@users.noreply.github.com>
2020-05-09 00:06:43 +0600

    created selectors for input and button

---
+++ app.js
@@ -0,0 +1,14 @@
+// create selectors for input and button 
+
+/* write a function countUniqueValues that accepts a sorted array, and counts the unique values in the array. There can be negative numbers in the array, but it always will be sorted.
+Examples: 
+countUniqueValues([1, 1, 1, 2]) // 2
+countUniqueValues([]) // 0
+countUniqueValues([-2, -1, -1, 0, 1]) // 4 */
+
+const inputByClassName = document.getElementsByClassName('todo-input')
+console.log('inputByClassName: ', inputByClassName)
+
+const buttonByClass = document.querySelector('button.todo-button')
+console.log('BUTTON by CLASS name: ', buttonByClass)
+
